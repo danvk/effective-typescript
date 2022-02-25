ce3dba8bbe46a293819f67e19b66100b9d495f69
yiminghe <yiminghe@gmail.com>
2022-02-25 17:27:50 +0800

    optimize item 50

--- samples/ch06-declarations/item-50-conditional-overload/conditional-overload-07.ts
+++ samples/ch06-declarations/item-50-conditional-overload/conditional-overload-07.ts
@@ -5,7 +5,7 @@ function double(x: any) { return x + x; }
 const num = double(12);  // number
 const str = double('x');  // string
 
-// function f(x: string | number): string | number
-function f(x: number|string) {
+// f<T extends number | string>(x: T): T extends string ? string : number
+function f<T extends number | string>(x: T) {
   return double(x);
 }
